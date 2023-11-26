73206df7db3101432c00e60afb32667eee1a6aa2
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-11-26 13:58:53 +0900

    [Fix] import가 두번 되어있던 오류 수정

--- src/components/modify/containers/ModifyBoardContainer.jsx
+++ src/components/modify/containers/ModifyBoardContainer.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import modify from "/assets/images/modify.png";
 import ModifySysModal from '@components/modify/ModifySysModal';
-import { useNavigate } from 'react-router-dom';
 
 
 const PageContainer = styled.div`
